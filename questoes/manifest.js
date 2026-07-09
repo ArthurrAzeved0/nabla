43ccd32de735addcc7ea95aa5b211efa5830997c
Arthur Azevedo <aea@poli.br>
2026-07-08 21:17:57 -0300

    Questões 13 a 38 da final de eletromag e revisão do curso teorico

--- questoes/manifest.js
+++ questoes/manifest.js
@@ -21,7 +21,7 @@ window.QUESTOES_MANIFEST = {
   eletromag: {
     "1ee": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html", "q07.html", "q08.html", "q09.html", "q10.html", "q11.html", "q12.html"],
     "2ee": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html", "q07.html", "q08.html", "q09.html", "q10.html", "q11.html", "q12.html"],
-    "final": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html", "q07.html", "q08.html", "q09.html", "q10.html", "q11.html", "q12.html"]
+    "final": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html", "q07.html", "q08.html", "q09.html", "q10.html", "q11.html", "q12.html", "q13.html", "q14.html", "q15.html", "q16.html", "q17.html", "q18.html", "q19.html", "q20.html", "q21.html", "q22.html", "q23.html", "q24.html", "q25.html", "q26.html", "q27.html", "q28.html", "q29.html", "q30.html", "q31.html", "q32.html", "q33.html", "q34.html", "q35.html", "q36.html", "q37.html", "q38.html"]
   },
   eqdiferenciais: {
     "1ee": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html", "q07.html", "q08.html", "q09.html", "q10.html", "q11.html", "q12.html", "q13.html", "q14.html", "q15.html", "q16.html"],
