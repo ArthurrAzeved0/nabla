aec6752c052bedbb88b03d416deb9acf804e02b2
Arthur Azevedo <aea@poli.br>
2026-07-11 15:11:57 -0300

    Expande banco de Cálculo 3 e reforça teoria de integrais
    - 1EE: q11-q16 (funções várias var, plano tangente, extremos, polares, Lagrange, área de superfície)
    - 2EE: q07-q17 cobrindo gaps da 2ª unidade (int. linha/superfície, Green, divergência, Stokes)
    - Final: q07-q12 "Estilo de prova" (esféricas, Gauss, Stokes, Lagrange)
    - Teoria seção 07: região tipo II, receitas de limites e conversão cilíndricas/esféricas
    - Bump cache-busting v=2

--- questoes/manifest.js
+++ questoes/manifest.js
@@ -14,9 +14,9 @@
    ========================================================================== */
 window.QUESTOES_MANIFEST = {
   calculo3: {
-    "1ee": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html", "q07.html", "q08.html", "q09.html", "q10.html"],
-    "2ee": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html"],
-    "final": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html"]
+    "1ee": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html", "q07.html", "q08.html", "q09.html", "q10.html", "q11.html", "q12.html", "q13.html", "q14.html", "q15.html", "q16.html"],
+    "2ee": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html", "q07.html", "q08.html", "q09.html", "q10.html", "q11.html", "q12.html", "q13.html", "q14.html", "q15.html", "q16.html", "q17.html"],
+    "final": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html", "q07.html", "q08.html", "q09.html", "q10.html", "q11.html", "q12.html"]
   },
   eletromag: {
     "1ee": ["q01.html", "q02.html", "q03.html", "q04.html", "q05.html", "q06.html", "q07.html", "q08.html", "q09.html", "q10.html", "q11.html", "q12.html"],
